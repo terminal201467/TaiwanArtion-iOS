9484e73c7870939f5db23e1c3681f3b47f28a3ff
MJ <terminal201467@gmail.com>
2022-04-23 13:55:09 +0800

    Feature(MapViewController)create the new file for the MapPage

---
+++ TaiwanArtion/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  TaiwanArtion
+//
+//  Created by Jhen Mu on 2022/4/23.
+//
+
+import UIKit
+
+class MapView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TaiwanArtion/MapViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MapViewModel.swift
+//  TaiwanArtion
+//
+//  Created by Jhen Mu on 2022/4/23.
+//
+
+import Foundation
