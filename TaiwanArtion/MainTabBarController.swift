85dde820585e496dd2bcf1c8fe55408aab606105
Timmy <a220009996@gmail.com>
2022-05-18 15:37:21 +0800

    feature(MainTabBarController) change tabBar image

--- TaiwanArtion/MainTabBarController.swift
+++ TaiwanArtion/MainTabBarController.swift
@@ -23,17 +23,17 @@ class MainTabBarController: UITabBarController {
         
         let findVC = FindExhibitionViewController()
         let findController = UINavigationController(rootViewController: findVC)
-        findController.tabBarItem.image = UIImage(systemName: "star.fill")
+        findController.tabBarItem.image = UIImage(named: "Find")
         findController.tabBarItem.title = "早找展覽"
         
         let nearbyVC = NearbyExhibitionViewController()
         let nearbyController = UINavigationController(rootViewController: nearbyVC)
-        nearbyController.tabBarItem.image = UIImage(systemName: "star.fill")
+        nearbyController.tabBarItem.image = UIImage(named: "Map")
         nearbyController.tabBarItem.title = "附近展覽"
         
         let shareVC = ShareExhibitionViewController()
         let shareController = UINavigationController(rootViewController: shareVC)
-        shareController.tabBarItem.image = UIImage(systemName: "star.fill")
+        shareController.tabBarItem.image = UIImage(named: "Share")
         shareController.tabBarItem.title = "分享展覽"
         
         viewControllers = [findController, nearbyController, shareController]
